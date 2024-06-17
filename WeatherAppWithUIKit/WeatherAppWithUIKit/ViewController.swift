1a4749cc3c54c2396b5aaabca49883c01b790d6e
thisisdana49 <thisisdana49@gmail.com>
2024-06-17 16:07:49 +0900

    [Feat] UITableView IBOutlet으로 연결 및 DataSource, Delegate 추가

--- WeatherAppWithUIKit/WeatherAppWithUIKit/ViewController.swift
+++ WeatherAppWithUIKit/WeatherAppWithUIKit/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hourTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hourCell", for: indexPath)
+      return cell
+    }
+    
+}
